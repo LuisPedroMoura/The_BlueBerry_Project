9a488ae130a9d8ef2ec587b40552bfb2a184d2d4
Luis Moura <luisppmoura@gmail.com>
2019-04-19 11:16:29 +0100

    just to update

--- android_app/app/src/main/java/com/bluebudget/bluebugdet/AppTransactionList.java
+++ android_app/app/src/main/java/com/bluebudget/bluebugdet/AppTransactionList.java
@@ -2,11 +2,13 @@ package com.bluebudget.bluebugdet;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AppTransactionList {
 
-    private List<AppTransaction> transactions = new ArrayList<>();
+    private Map<Integer, AppTransaction> transactions = new HashMap<>();
 
 
     public AppTransactionList() {}
@@ -16,21 +18,21 @@ public class AppTransactionList {
                           String wallet) {
         AppTransaction t = new AppTransaction(value, date, category, notes, location, wallet,
                 null, AppTransactionType.INCOME);
-        transactions.add(t.getId(), t);
+        transactions.put(t.getId(), t);
     }
 
     public void addExpense(double value, Date date, String category, String notes , String location,
                           String wallet) {
         AppTransaction t = new AppTransaction(-value, date, category, notes, location, wallet,
                 null, AppTransactionType.EXPENSE);
-        transactions.add(0, t);
+        transactions.put(t.getId(), t);
     }
 
     public void addTransfer(double value, Date date, String category, String notes , String location,
                           String wallet, String recipientWallet) {
         AppTransaction t = new AppTransaction(-value, date, category, notes, location, wallet,
                 recipientWallet, AppTransactionType.TRANSFER);
-        transactions.add(t.getId(), t);
+        transactions.put(t.getId(), t);
     }
 
     public void removeTransaction(int id){
@@ -49,7 +51,9 @@ public class AppTransactionList {
 
         List<AppTransaction> res = new ArrayList<>();
 
-        for (AppTransaction tr : this.transactions){
+        for (Integer id : this.transactions.keySet()){
+
+            AppTransaction tr = this.transactions.get(id);
 
             if (minDate == null){
                 minDate = new Date(0,0,0);
