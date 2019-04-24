7cdd7c0c084931e752443d103cb5923796954fa6
Luis Moura <luisppmoura@gmail.com>
2019-04-24 11:38:58 +0100

    micro correction to create_db script

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,24 +1,24 @@
 CREATE TABLE Project.money_accounts
 (
+	id INT IDENTITY(1,1),
 	account_name VARCHAR(20),
 	patrimony INT DEFAULT 0,
-	id INT IDENTITY(1,1),
 	CHECK (account_name!=''),
-	CONSTRAINT PK_MONEYACCOUNTS PRIMARY KEY (account_name, id)
+	CONSTRAINT PK_MONEYACCOUNTS PRIMARY KEY (id)
 );
 
 CREATE TABLE Project.wallets
 (
-	balance MONEY,
-	[name] VARCHAR(20),
 	id INT IDENTITY(1,1),
-	account_name VARCHAR(20),
+	[name] VARCHAR(20),
 	account_id INT,
+	balance MONEY,	
 	CONSTRAINT PK_WALLETS PRIMARY KEY (id),
-	CONSTRAINT FK_WALLETS_MONEYACCOUNTS FOREIGN KEY (account_name, account_id) REFERENCES Project.money_accounts(account_name, id)
+	CONSTRAINT FK_WALLETS_MONEYACCOUNTS FOREIGN KEY (account_id) REFERENCES Project.money_accounts(id)
 		ON DELETE NO ACTION ON UPDATE CASCADE
 );
-DECLARE @IdentityOutput table ( ID int )
+DECLARE @IdentityOutput table ( ID int );
+
 
 CREATE TABLE Project.recurrence
 (
@@ -45,14 +45,10 @@ CREATE TABLE Project.subscriptions
 		ON DELETE NO ACTION ON UPDATE CASCADE	
 );
 
-select * from Project.users;
-select * from project.subscriptions;
-delete from Project.users where [user_name] like 'user%';
-
 CREATE TABLE Project.users
 (
-	[user_name] VARCHAR(20),
 	email VARCHAR(50),
+	[user_name] VARCHAR(20),
 	CHECK([user_name] != ''),
 	CONSTRAINT PK_USERS PRIMARY KEY (email),
 );
@@ -60,25 +56,22 @@ CREATE TABLE Project.users
 CREATE TABLE Project.users_money_accounts
 (
 	user_email VARCHAR(50),
-	account_name VARCHAR(20),
 	account_id INT,
-	CONSTRAINT PK_USERSMONEYACCOUNTS PRIMARY KEY (user_email, account_name, account_id),
+	CONSTRAINT PK_USERSMONEYACCOUNTS PRIMARY KEY (user_email, account_id),
 	CONSTRAINT FK_USERSMONEYACCOUNTS_USERS FOREIGN KEY (user_email) REFERENCES Project.users(email)
 		ON DELETE NO ACTION ON UPDATE CASCADE,
-	CONSTRAINT FK_USERSMONEYACCOUNTS_MONEYACCOUNTS FOREIGN KEY (account_name, account_id) REFERENCES Project.money_accounts(account_name, id)
-		ON DELETE NO ACTION ON UPDATE CASCADE
-		
+	CONSTRAINT FK_USERSMONEYACCOUNTS_MONEYACCOUNTS FOREIGN KEY (account_id) REFERENCES Project.money_accounts(id)
+		ON DELETE NO ACTION ON UPDATE CASCADE		
 );
 
 CREATE TABLE Project.categories
 (
-	[name] VARCHAR(20),
 	id INT,
-	account_name VARCHAR(20),
+	[name] VARCHAR(20),
 	account_id INT,
 	CHECK([name] != ''),
-	CONSTRAINT PK_CATEGORIES PRIMARY KEY (id, account_name, account_id),
-	CONSTRAINT FK_CATEGORIES_MONEYACCOUNTS FOREIGN KEY (account_name, account_id) REFERENCES Project.money_accounts(account_name, id)
+	CONSTRAINT PK_CATEGORIES PRIMARY KEY (id, account_id),
+	CONSTRAINT FK_CATEGORIES_MONEYACCOUNTS FOREIGN KEY (account_id) REFERENCES Project.money_accounts(id)
 		ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
@@ -91,19 +84,18 @@ CREATE TABLE Project.transaction_types
 
 CREATE TABLE Project.transactions
 (
+	id INT IDENTITY(1,1),
 	amount INT,
 	[date] DATETIME,
 	notes VARCHAR(50),
-	id INT IDENTITY(1,1),
 	[location] VARCHAR(50),
 	category_id INT,
-	account_name VARCHAR(20),
 	account_id INT,
 	transaction_type_id INT,
 	wallet_id INT,
 	CHECK(amount>0),
 	CONSTRAINT PK_TRANSACTIONS PRIMARY KEY (id),
-	CONSTRAINT FK_TRANSACTIONS_CATEGORIES FOREIGN KEY (category_id, account_name, account_id) REFERENCES Project.categories(id, account_name, account_id)
+	CONSTRAINT FK_TRANSACTIONS_CATEGORIES FOREIGN KEY (category_id, account_id) REFERENCES Project.categories(id, account_id)
 		ON DELETE NO ACTION ON UPDATE CASCADE,
 	CONSTRAINT FK_TRANSACTIONS_TRANSACTIONTYPES FOREIGN KEY (transaction_type_id) REFERENCES Project.transaction_types(id)
 		ON DELETE NO ACTION ON UPDATE CASCADE,
@@ -124,48 +116,45 @@ CREATE TABLE Project.transfers
 
 CREATE TABLE Project.budgets
 (
-	amount INT DEFAULT 0,
 	id INT IDENTITY(1,1),
+	category_id INT,
+	account_id INT,
+	amount INT DEFAULT 0,
 	[start_date] DATETIME,
 	end_date DATETIME,
 	periodicity INT,
-	category_id INT,
-	account_name VARCHAR(20),
-	account_id INT,
-	CONSTRAINT PK_BUDGETS PRIMARY KEY (id, category_id, account_name, account_id),
+	CONSTRAINT PK_BUDGETS PRIMARY KEY (id, category_id, account_id),
 	CONSTRAINT FK_BUDGETS_RECURRENCE FOREIGN KEY (periodicity) REFERENCES Project.recurrence(periodicity)
 		ON DELETE NO ACTION ON UPDATE CASCADE,
-	CONSTRAINT FK_BUDGETS_CATEGORIES FOREIGN KEY (category_id, account_name, account_id) REFERENCES Project.categories(id, account_name, account_id)
+	CONSTRAINT FK_BUDGETS_CATEGORIES FOREIGN KEY (category_id, account_id) REFERENCES Project.categories(id, account_id)
 		ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
 CREATE TABLE Project.goals
 (
+	[name] VARCHAR(20),
+	category_id INT,
+	account_id INT,
 	amount INT DEFAULT 0,
 	term DATETIME,
 	accomplished INT,
-	[name] VARCHAR(20),
-	category_id INT,
-	account_name VARCHAR(20),
-	account_id INT,
 	CHECK([name] != ''),
-	CONSTRAINT PK_GOALS PRIMARY KEY ([name], category_id, account_name, account_id),
-	CONSTRAINT FK_GOALS_CATEGORIES FOREIGN KEY (category_id, account_name, account_id) REFERENCES Project.categories(id, account_name, account_id)
+	CONSTRAINT PK_GOALS PRIMARY KEY ([name], category_id, account_id),
+	CONSTRAINT FK_GOALS_CATEGORIES FOREIGN KEY (category_id, account_id) REFERENCES Project.categories(id, account_id)
 		ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
 CREATE TABLE Project.loans
 (
+	[name] VARCHAR(20),
+	account_id INT,
 	amount MONEY,
 	term DATETIME,
 	interest DECIMAL(5,2),
-	[name] VARCHAR(20),
 	montlhy_payment MONEY,
-	account_name VARCHAR(20),
-	account_id INT,
 	CHECK([name] != ''),
-	CONSTRAINT PK_LOANS PRIMARY KEY ([name], account_name, account_id),
-	CONSTRAINT FK_LOANS_MONEYACCOUNTS FOREIGN KEY (account_name, account_id) REFERENCES Project.money_accounts(account_name, id)
+	CONSTRAINT PK_LOANS PRIMARY KEY ([name], account_id),
+	CONSTRAINT FK_LOANS_MONEYACCOUNTS FOREIGN KEY (account_id) REFERENCES Project.money_accounts(id)
 		ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
@@ -179,8 +168,8 @@ CREATE TABLE Project.stock_types
 
 CREATE TABLE Project.stocks
 (
-	bid_price INT,
 	company VARCHAR(50),
+	bid_price INT,
 	ask_price INT,
 	stock_type_id INT,
 	CHECK(company != ''),
@@ -191,20 +180,21 @@ CREATE TABLE Project.stocks
 
 CREATE TABLE Project.purchased_stocks
 (
-	purchase_value INT,
 	ticker INT IDENTITY(1,1),
-	quantity INT,
 	company VARCHAR(50),
-	account_name VARCHAR(20),
 	account_id INT,
+	purchase_value INT,
+	quantity INT,
 	CONSTRAINT PK_PURCHASEDSTOCKS PRIMARY KEY (ticker),
 	CONSTRAINT FK_PURCHASEDSTOCKS_STOCKS FOREIGN KEY (company) REFERENCES Project.stocks(company)
 		ON DELETE NO ACTION ON UPDATE CASCADE,
-	CONSTRAINT FK_PURCHASEDSTOCKS_MONEYACCOUNTS FOREIGN KEY (account_name, account_id) REFERENCES Project.money_accounts(account_name, id)
+	CONSTRAINT FK_PURCHASEDSTOCKS_MONEYACCOUNTS FOREIGN KEY (account_id) REFERENCES Project.money_accounts(id)
 		ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
-
+--------------------------------------------------------------------
+-- INSERTION OF BASIC VALUES ---------------------------------------
+--------------------------------------------------------------------
 INSERT INTO Project.recurrence (designation, periodicity)
 VALUES 	
 		('monthly', 1),
@@ -223,9 +213,3 @@ INSERT INTO Project.stock_types (designation,id)
 VALUES 	
 		('common', 0),
 		('preferred', 1);
-
-
--- HAVE TO CHANGE DATE 
-select * from Project.users;
-select * from Project.subscriptions
-SELECT periodicity FROM Project.recurrence WHERE (designation='quarterly')
